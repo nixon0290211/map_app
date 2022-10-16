7bda32f05fcb0d17287d6f6e608682cc49641337
chibatakashi <nixon0290211@icloud.com>
2022-10-16 11:10:58 +0900

    CRUD処理

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.main')
+
 @section('title', '店舗一覧')
+
 @section('content')
-<h1>店舗一覧</h1>
+    <h1>店舗一覧</h1>
+
+    <ul>
+        @foreach ($shops as $shop)
+            <li><a href="{{ route('shops.show', $shop) }}">{{ $shop->name }}</a></li>
+        @endforeach
+    </ul>
+
+    <a href="{{ route('shops.create') }}">create</a>
 @endsection
